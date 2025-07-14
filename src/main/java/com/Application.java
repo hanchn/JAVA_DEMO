0c73ab85bd285e95f50f4be8cd2e1f648f1d8b71
yuanjing <jing.yuan@kapeixi.com>
2025-07-14 17:22:26 +0800

    .

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,4 +1,4 @@
-package com.example.todolist;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +8,12 @@ import com.example.todolist.config.AppConstants;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppConstants.class)
-public class TodolistDemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(TodolistDemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
         System.out.println("Hello World !");
+        System.out.println("测试热更新 !");
     }
 
 }
